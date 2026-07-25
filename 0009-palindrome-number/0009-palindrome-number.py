0a5aa558a3a0c3c29884aa3cefa41660095756a0
Jasvanth <diamondjasvanth17112004@gmail.com>
2026-07-25 11:19:56 +0530

    Practice: Time: 0 ms (100%), Space: 19.3 MB

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        s=str(x)
+        y=s[::-1]
+        return s==y
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
