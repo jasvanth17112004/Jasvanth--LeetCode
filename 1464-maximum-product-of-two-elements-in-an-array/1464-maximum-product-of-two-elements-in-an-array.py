0026e1e01244d2f88b007826586904eacfd03668
Jasvanth <diamondjasvanth17112004@gmail.com>
2026-07-27 10:38:24 +0530

    Day 75: Time: 3 ms (34.83%), Space: 19.4 MB (

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.py
@@ -0,0 +1,10 @@
+# this has been don eby me.Jasvanth
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        nums.sort()
+        return ((nums[-1]-1)*(nums[-2]-1))
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
