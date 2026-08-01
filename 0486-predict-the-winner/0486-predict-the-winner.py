7146623818e55a0577ef423c3666a37f2efbb26f
Jasvanth <diamondjasvanth17112004@gmail.com>
2026-08-01 12:28:07 +0530

    Day 80: Time: 3 ms (55.77%), Space: 19.8 MB

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,26 @@
+#this has been done by me with a lot of help.Jasvanth.
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+
+        memo = {}
+
+        def solve(left, right):
+
+            if left == right:
+                return nums[left]
+
+            if (left, right) in memo:
+                return memo[(left, right)]
+
+            take_left = nums[left] - solve(left + 1, right)
+            take_right = nums[right] - solve(left, right - 1)
+
+            memo[(left, right)] = max(take_left, take_right)
+
+            return memo[(left, right)]
+
+        return solve(0, len(nums) - 1) >= 0
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
